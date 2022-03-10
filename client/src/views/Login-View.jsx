833525e2683855b9df08215d8f89fd3ff7afa6c6
Yeasir H <yeasir01@gmail.com>
2022-03-10 02:52:27 -0800

    react stuff  *\_( ^ ^ )_/*

---
+++ client/src/views/Login-View.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import LoginComponent from "../components/Login-Component";
+
+const LoginView = () => {
+    
+    return(
+        <>
+            <LoginComponent />
+        </>
+    );
+};
+
+export default LoginView;
